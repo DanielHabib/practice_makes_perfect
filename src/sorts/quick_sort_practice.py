07c100fd151fcefdf6cbb9c7d2b2cde7e05d523b
DanielHabib <d.g.habib7@gmail.com>
2015-08-12 22:35:21 -0400

    Quick sort

--- src/sorts/quick_sort_practice.py
+++ src/sorts/quick_sort_practice.py
@@ -240,6 +240,86 @@ def partition5(alist, first, last):
     alist[first] = temp
     return rightmark
 
+
+
+
+
+
+
+
+
+
+def quicksort7(alist):
+    quicksort_helper7(alist, 0, len(alist) - 1)
+
+def quicksort_helper7(alist, first, last):
+    if len(alist) > 1:
+        split_point = partition7(alist, first, last)
+        quicksort_helper7(alist, first, split_point - 1)
+        quicksort_helper7(alist, first, split_point + 1)
+
+def partition7(alist, first, last):
+    done = False
+    pivot_value = alist[first]
+    leftmark = first + 1
+    rightmark = last
+    while not done:
+        while leftmark <= rightmark and alist[leftmark] <= pivot_value:
+            leftmark = leftmark + 1
+        while rightmark >= leftmark and alist[rightmark] >= pivot_value:
+            rightmark = rightmark - 1
+
+        if leftmark > rightmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+    temp = alist[rightmark]
+    alist[rightmark] = pivot_value
+    alist[first] = temp
+    return rightmark
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 alist = [1,2,3,45,5,36,45,65,47,45,7645,7,457,54,6,456,45,6,2,34]
 
 if __name__ == '__main__':
