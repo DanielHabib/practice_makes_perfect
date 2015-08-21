78337fb28bd271c2e55962bddb690c0af82f8659
DanielHabib <d.g.habib7@gmail.com>
2015-08-20 20:35:53 -0400

    QS: 5:19

--- src/sorts/quick_sort_practice.py
+++ src/sorts/quick_sort_practice.py
@@ -324,9 +324,61 @@ def partition8(alist, first, last):
 
 
 
+
+
+
+def qs9(alist):
+    qs9_helper(alist, 0, len(alist) - 1)
+
+def qs9_helper(alist, first, last):
+    if first > last: # Why?
+        split_point = partition9(alist, first, last)
+        qs9_helper(alist, first, split_point - 1)
+        qs9_helper(alist, split_point + 1, last)
+
+
+def partition9(alist, first, last):
+    done = False
+    pivot_value = alist[first]
+    leftmark = first + 1
+    rightmark = last
+    while not done:
+        while leftmark<=rightmark and alist[leftmark]<=pivot_value:
+            leftmark = leftmark + 1
+        while leftmark<=rightmark and alist[rightmark] >= pivot_value:
+            rightmark = rightmark - 1
+
+        if leftmark > rightmark: # Why
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 alist = [1,2,3,45,5,36,45,65,47,45,7645,7,457,54,6,456,45,6,2,34]
-
-
 
 if __name__ == '__main__':
     qs8(alist)
