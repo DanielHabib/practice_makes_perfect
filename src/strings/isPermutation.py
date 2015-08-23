8c1ac3bb3f186d531fb9ff5613c08c1922185453
DanielHabib <d.g.habib7@gmail.com>
2015-08-23 19:05:42 -0400

    isPermutation: 1.2

--- src/strings/isPermutation.py
+++ src/strings/isPermutation.py
@@ -9,3 +9,21 @@ def isPermuation(string1, string2):
         TC:
 
     """
+    if len(string1) != len(string2):
+        return False
+    valDict = {}
+    for val in string1:
+        try:
+            valDict[val] = valDict[val] + 1
+        except:
+            valDict[val] = 0
+    for val in string2:
+        try:
+            if valDict[val] == 0:
+                return False
+            else:
+                valDict[valDict] = valDict[val] - 1
+
+        except:
+            return False
+    return True
