70fb2e30d5301a7e229fd90e7e6f67db037778ba
DanielHabib <d.g.habib7@gmail.com>
2015-08-24 22:05:01 -0400

    palindrome and permutation: 1.4

---
+++ src/strings/isPermutationAndPalindrome.py
@@ -0,0 +1,23 @@
+""" 1.4 """
+
+"""
+    Problem:
+        Given a string return all permutations that are palindromes
+
+    BCR: O(N)
+
+    Steps:
+        Prelim: ASCII or Unicode? assume Unicode
+        1. Loop through once and put the amount of each letter into a dictionary
+        2. loop through all values in dictionary making sure that all
+            values are even except for one, keep note of that pivot value
+            2.5. The value can be any odd value as long as there is only 1
+                odd value
+        3. place the pivot value in the middle
+        4. Then we want to take stock, create an array of half of the values
+            we have recorded in the dictionary.
+        4. Now we must find all the permutations of half the array
+
+
+"""
+
