57a93d758060584e9684192285175e1e850bc305
DanielHabib <d.g.habib7@gmail.com>
2015-08-24 22:06:21 -0400

    palindrome and permutation: 1.4

--- src/strings/isPermutationAndPalindrome.py
+++ src/strings/isPermutationAndPalindrome.py
@@ -8,7 +8,8 @@
 
     Steps:
         Prelim: ASCII or Unicode? assume Unicode
-        1. Loop through once and put the amount of each letter into a dictionary
+        1. Loop through once and put the amount of each letter into a 
+            dictionary
         2. loop through all values in dictionary making sure that all
             values are even except for one, keep note of that pivot value
             2.5. The value can be any odd value as long as there is only 1
@@ -16,7 +17,9 @@
         3. place the pivot value in the middle
         4. Then we want to take stock, create an array of half of the values
             we have recorded in the dictionary.
-        4. Now we must find all the permutations of half the array
+        4. Now we must find all the permutations of half the array.
+        5. Whats the fastest way to find all of the permutations of half of
+            the array?
 
 
 """
