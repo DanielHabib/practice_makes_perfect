d0f017951ee24898ece3480c34473e896906c810
DanielHabib <d.g.habib7@gmail.com>
2015-08-25 23:25:50 -0400

    String Compression: 1.6

--- src/strings/stringCompression.py
+++ src/strings/stringCompression.py
@@ -1,5 +1,7 @@
 """ 1.6 """
 
+from unittest import TestCase
+
 """
     Time: 12:59
     Problem: given 'aabccca' return 'a2b1c3a1', if the string is unchanged,
@@ -20,7 +22,9 @@ def stringCompression(str1):
     """
     array1 = takeStock(str1)
     if len(array1) == len(str1):
-        return str1
+        for l1 in array1:
+            if l1[2] != 1:
+                return str1
     return createString(array1)
 
 def takeStock(str1):
@@ -28,15 +32,36 @@ def takeStock(str1):
     goldIndex = -1
     array1 = []
 
-    for index, val in enumerate str1:
+    for index, val in enumerate(str1):
         if val == gold:
             elem = array1[goldIndex]
             elem[1] = elem[1] + 1
         else:
             goldIndex + 1
             elem = [val, 1]
-            array1[goldIndex] = elem
+            array1.append(elem)
     return array1
 
 
 def createString(array1):
+    str1 = ""
+    for elem in array1:
+        str1 += elem[0] + str(elem[1])
+    return str1
+
+
+class testStringCompression(TestCase):
+
+    def testFullSuccess(self):
+        str1 = "aabccca"
+        expectStr = "a2b1c3a1"
+
+        result = stringCompression(str1)
+
+        self.assertEqual(result, expectStr)
+
+    def testSpecialCase(self):
+        str1 = "abcdefg"
+        result = stringCompression(str1)
+        self.assertEqual(result, str1)
+
