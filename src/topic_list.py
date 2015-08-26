f9ded48c5992841ee5392cd7b0ec65bccaecb942
Daniel Habib <d.g.habib7@gmail.com>
2015-08-26 07:58:13 -0400

    Delete topic_list.py

--- src/topic_list.py
+++
@@ -1,8 +0,0 @@
-""" The Topic List """
-
-
-class TopicList(object):
-    """ Where I list the concepts that need more work """
-    the_list = ['String types', 'Space Complexity']
-    understood = []
-
