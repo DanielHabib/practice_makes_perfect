7d0ff524700e53d30bd7886a36575c815f21a3ba
DanielHabib <d.g.habib7@gmail.com>
2015-09-23 23:25:23 -0400

    Tree DS

--- src/datastructures/tree.py
+++ src/datastructures/tree.py
@@ -3,6 +3,47 @@ class Tree:
         self.root = node
         self.nodes = []
 
+    def firstCommonAncestor(a, b):
+        """ 4.8
+            Take two nodes and find their first common ancestor without
+            using any additional datastructures
+            Time Complexity: O(log(N)) best case, and O(N) worst case
+            Space Complexity: constant
+        """
+        while a.parent and b.parent:
+            if a.isMarked():
+                return a
+            a.mark()
+            if b.isMarked():
+                return b
+            b.mark()
+            a = a.parent
+            b = b.parent
+
+        while a.parent:
+            if a.marked():
+                return a
+            a.mark()
+            a = a.parent
+        while b.parent:
+            if b.marked():
+                return b
+            b.mark()
+            b = b.parent
+        return None
+
+
 class Node:
-    def __init__(self, val):
+    def __init__(self, val, children=None, parent=None):
         self.val = val
+        self.children = children
+        self.parent = parent
+        self.marked = False
+
+    def mark(self):
+        """ Mark the Node """
+        self.marked = True
+
+    def isMarked(self):
+        """ Check if the Node is Marked """
+        return self.marked
--- src/graphs/mistakes.md
+++ src/graphs/mistakes.md
@@ -1,7 +1,9 @@
 ## Things to pay attention when dealing with Graphs
 * Is this search? If so whether to use BFS or DFS
+* Always ask what details are available on the nodes
 
 ## General Takes Aways
+* Understanding the Time Complexity associated with Trees and Graphs is crucial to understanding why they are so important
 
 ## Mistakes
 * BST's are smallest to the left and larger to the right, not the other way around
