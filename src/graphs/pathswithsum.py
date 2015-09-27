ff4977c06aa0274a01609fc72b56b2fea4bf7278
DanielHabib <d.g.habib7@gmail.com>
2015-09-27 18:11:56 -0400

    Check sums 4.12

--- src/graphs/pathswithsum.py
+++ src/graphs/pathswithsum.py
@@ -13,7 +13,24 @@
 
         Would keeping track of the sum up to a certain node make any sense?
 
+        I need to effectively maintain a record of all sums up to this
+        specific node and then apply this node against all of those specific
+        sums to see if it adds up, and add +1 to the counter
+
 
     BCR:O(N)
 """
-def paths_with_sum()
+def paths_with_sum(tree, val):
+    """Time Complexity: O(Nlog(N))"""
+    paths_with_sum_helper(node, [], 0, val)
+
+def paths_with_sum_helper(node, sums_array, counter, check_val):
+    for index, val in enumerate(sums_array):
+        new_val = val + node.val
+        sums_array[index] = new_val
+        if new_val == check_val:
+            counter = counter + 1
+    sums_array.append(node)
+
+    paths_with_sum_helper(node.leftChild, sums_array, counter, check_val)
+    paths_with_sum_helper(node.rightChild, sums_array, counter, check_val)
