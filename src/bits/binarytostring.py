88b359318a5aa3e342bbde9747496a04894fca9c
DanielHabib <d.g.habib7@gmail.com>
2015-09-27 23:54:45 -0400

    Start Binary to string

---
+++ src/bits/binarytostring.py
@@ -0,0 +1,10 @@
+""" Binary to string 5.2 """
+"""
+    Given:
+        Real number between 0 and 1 (decimal) passed in as a double
+    Action:
+        print binary representation otherwise throw error
+    Output:
+        At most 32 Characters
+
+"""
