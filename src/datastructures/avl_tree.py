473d1d879dc65c6b1ea8e8f02b1c1dbf2e049dc8
DanielHabib <d.g.habib7@gmail.com>
2015-10-12 22:10:19 -0400

    Avl Tree

--- src/datastructures/avl_tree.py
+++ src/datastructures/avl_tree.py
@@ -1,8 +1,11 @@
 """ An Avl tree that balances a tree as nodes are added on """
 from node import AVLNode
 
+from unittest import TestCase
+
+
 class DuplicateNodeError(Exception):
-    """ """
+    """ Attempted to enter a duplicate node """
 
 class AVLTree:
     """ AVL Tree that is self balancing
@@ -12,7 +15,6 @@ class AVLTree:
         self.root = AVLNode(root)
         self.nodes = []
         self.height = height
-
 
     def inject(self, data):
         """
@@ -34,22 +36,47 @@ class AVLTree:
             self.rebalance()
             return True
 
-
     def inject_helper(self, current_node, data):
         """ Binary Search Tree Injection """
         if data == current_node.data:
             raise DuplicateNodeError
         if current_node.data < data:
             if not current_node.left:
-                current_node.left = AVLNode(data)
+                new_node = AVLNode(data)
+                current_node.left = new_node
+                self.nodes.append(new_node)
             return self.inject_helper(current_node.left, data)
         else:
             if not current_node.right:
-                current_node.right = AVLNode(data)
+                new_node = AVLNode(data)
+                current_node.right = new_node
+                self.nodes.append(new_node)
             return self.inject_helper(current_node.right, data)
 
 
     def rebalance(self):
+        pass
+
+    def __repr__(self):
+        return self.nodes;
+
+
+class TestAVLTree(TestCase):
+    """ Test the avl tree to ensure proper behavior """
+
+    def test_inject_on_empty_tree_sets_root(self):
+        """ Be sure that we can inject """
+        tree = AVLTree()
+        tree.inject(1)
+        self.assertEquals(tree.root.data, 1)
+
+    def test_inject_right_node(self):
+        """ To ensure that nodes are placed in the right side correctly """
+        tree = AVLTree()
+        tree.inject(1)
+        tree.inject(3)
+        self.assertEquals(tree.root.right.data, 3)
+
 
 
 
