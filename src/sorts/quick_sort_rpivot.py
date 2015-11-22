3be7f3c8d10eb417f1c5c1d6ee80bc8e42ddab8a
DanielHabib <d.g.habib7@gmail.com>
2015-11-22 14:58:10 -0500

    More qsort practice

--- src/sorts/quick_sort_rpivot.py
+++ src/sorts/quick_sort_rpivot.py
@@ -67,7 +67,70 @@ def partition_2(alist, first, last):
 
 def swap_2(array, a, b):
     array[a], array[b] = array[b], array[a]
+
+
+
+
+def qsort3(alist):
+    qsort3_helper(alist, 0, len(alist) - 1)
+
+def qsort3_helper(alist, first, last):
+    if last > first:
+        pivot = partition_3(alist, first, last)
+        qsort3_helper(alist, first, pivot - 1)
+        qsort3_helper(alist, pivot + 1, last)
+
+def partition_3(alist, first, last):
+    pivot = first + random.randrange(last - first - 1)
+    swap_3(alist, pivot, last)
+    for i in range(first, last):
+        if alist[i] <= alist[last]:
+            swap(alist, i, first)
+            first += 1
+    swap(alist, first, last)
+    return first
+
+def swap_3(alist, a, b):
+    alist[a], alist[b] = alist[b], alist[a]
+
+
+def qsort4(alist):
+    qsort4_helper(alist, 0 ,len(alist) - 1)
+
+def qsort4_helper(alist, first, last):
+    if last > first:
+        split_point = partition_4(alist, first, last)
+        qsort4_helper(alist, first, split_point - 1)
+        qsort4_helper(alist, split_point + 1, last)
+
+
+def partition_4(alist, first, last):
+    pivot = first + random.randrange(last - first + 1)
+    swap(alist, pivot, last)
+    for i in range(first, last):
+        if alist[i] <= alist[last]:
+            swap(alist, i, first)
+            first += 1
+    swap(alist, first, last)
+    return first
+
+
+def swap_4(alist, a, b):
+    alist[a], alist[b] = alist[b], alist[a]
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     alist = [3,2,4,15,6,7,2,3,5,7,2,3,2,6]
-    qsort2(alist)
+    qsort4(alist)
     print(alist)
