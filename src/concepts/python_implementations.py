c4cbc549a58b2fda6ad6e4b8d26c481d858add97
Daniel Habib <d.g.habib7@gmail.com>
2016-11-21 17:05:49 -0500

    Some python implementation info

---
+++ src/concepts/python_implementations.py
@@ -0,0 +1,13 @@
+"""
+Pyhton implementations.
+
+The Reference implementation in python is C Python
+A reference implementation is the main implementation that others are based off of.
+Different python implementations are used to improve performance and help interface with differnet programming languages.
+
+For exampe in pypy, at the cost of being incapadable with certain libraries, you gain a 5 x performance increase.
+
+Jython is used if you need to import java modules to your code.
+Iron Python and PythonNet are used to interface with .NET libraries
+
+"""
